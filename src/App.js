bfa8b9a68f5326d480efb54dbe5b4d3fd857b6a5
JessenDGibbs <jessen.gibbs@mail.mcgill.ca>
2024-04-21 20:48:30 -0400

    post6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Post from './components/Post';
+import Home from './Home';
+import './App.css';
+
 import Post1 from './posts/Post1';
 import Post2 from './posts/Post2';
 import Post3 from './posts/Post3';
 import Post4 from './posts/Post4';
-
-
-
-import Home from './Home';
-import './App.css';
 import Post5 from './posts/Post5';
+import Post6 from './posts/Post6';
+
+
 
 function App() {
   return (
@@ -23,10 +23,11 @@ function App() {
           <Route path="/Post3" element={<Post3 />} />
           <Route path="/Post4" element={<Post4 />} />
           <Route path="/Post5" element={<Post5 />} />
-
+          <Route path="/Post6" element={<Post6 />} />
 
         </Routes>
         <div className="links">
+        <Link to="/Post6">Read this post</Link>
         <Link to="/Post5">Read this post</Link>
         <Link to="/Post4">Read this post</Link>
         <Link to="/Post3">Read this post</Link>
---
+++ src/posts/Post6.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Post6() {
+  return (
+    <div style={{ width: "50%" }}>
+      <b>Amidst war, localize the internet: </b>Destroying the internet would be a strong play amidst a global war. 
+      It would level the playing field. Data centers and networks would become focal points for attacks in an effort to 
+      slow communication and divide players. Domestic unrest would lead to internal instability 
+      of global super-powers and weaken their frontline efforts. Smaller players would 
+      benefit. Localizing the internet, and controlling mass information access would be 
+      one way to resist this risk. As the global internet falls, individuals should seek 
+      to take as much information as possible into their own, local, secure, and self-managed 
+      systems. As  global architectures and netwoks become more risky local internets and 
+      information management will boom. SImilarly accessible hardware than can run it will 
+      also boom.
+    </div>
+  );
+}
+
+export default Post6;
